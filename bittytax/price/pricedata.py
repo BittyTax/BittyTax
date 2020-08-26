BittyTax/BittyTax

commit c59b7cf316379c8a2781b8cfeecaa4e05402bf40

Incorrect file committed for change badf5a1da8257553201aabb2520d11fd66a7c849.

bittytax/price/pricedata.py
@@ -72,7 +72,6 @@ class PriceData(object):
             raise UnexpectedDataSourceError(data_source)
 
     def get_latest(self, asset, quote):
-        price = name = data_source = None
         for data_source in self.data_source_priority(asset):
             price, name = self.get_latest_ds(data_source, asset, quote)
             if price is not None:
@@ -84,12 +83,11 @@ class PriceData(object):
                         quote,
                         self.data_sources[data_source.upper()].name(),
                         name))
-                break
+                return price, name, self.data_sources[data_source.upper()].name()
 
-        return price, name, self.data_sources[data_source.upper()].name()
+        return None, None, None
 
     def get_historical(self, asset, quote, timestamp):
-        price = name = data_source = url = None
         for data_source in self.data_source_priority(asset):
             price, name, url = self.get_historical_ds(data_source, asset, quote, timestamp)
             if price is not None:
@@ -102,6 +100,6 @@ class PriceData(object):
                         quote,
                         self.data_sources[data_source.upper()].name(),
                         name))
-                break
+                return price, name, self.data_sources[data_source.upper()].name(), url
 
-        return price, name, self.data_sources[data_source.upper()].name(), url
+        return None, None, None, None

bittytax/price/bittytax_price.py
@@ -67,9 +67,7 @@ def main():
         if config.args.date:
             try:
                 timestamp = dateutil.parser.parse(config.args.date)
-                timestamp = timestamp.replace(tzinfo=config.TZ_UTC)
-                print("%s" % timestamp.strftime('%Y-%m-%dT%H:%M:%S %Z'))
-            except ValueError:
+            except ValueError as e:
                 if sys.version_info[0] < 3:
                     err_msg = ' '.join(e)
                 else:

commit 681f6084f24a30f4bb7567a62c32f7b9c37c3251

Fixed the asset (symbol) name was not being returned
If the specific price data does not exist, but the asset is supported by the data source, the asset name should be returned.

This bug was introduced by commit: c59b7cf316379c8a2781b8cfeecaa4e05402bf40.

bittytax/price/pricedata.py
@@ -72,6 +72,7 @@ class PriceData(object):
             raise UnexpectedDataSourceError(data_source)
 
     def get_latest(self, asset, quote):
+        name = None
         for data_source in self.data_source_priority(asset):
             price, name = self.get_latest_ds(data_source, asset, quote)
             if price is not None:
@@ -84,10 +85,10 @@ class PriceData(object):
                         self.data_sources[data_source.upper()].name(),
                         name))
                 return price, name, self.data_sources[data_source.upper()].name()
-
-        return None, None, None
+        return None, name, None
 
     def get_historical(self, asset, quote, timestamp):
+        name = None
         for data_source in self.data_source_priority(asset):
             price, name, url = self.get_historical_ds(data_source, asset, quote, timestamp)
             if price is not None:
@@ -101,5 +102,4 @@ class PriceData(object):
                         self.data_sources[data_source.upper()].name(),
                         name))
                 return price, name, self.data_sources[data_source.upper()].name(), url
-
-        return None, None, None, None
+        return None, name, None, None