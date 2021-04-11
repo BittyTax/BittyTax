8b87919a9e92e448b527ff2b2627973cc41fbb94
beirut-boop <78885860+beirut-boop@users.noreply.github.com>
2021-04-11 03:44:28 +0200

    added support for syndicate payouts

--- bittytax/conv/parsers/cryptocom_exchange.py
+++ bittytax/conv/parsers/cryptocom_exchange.py
@@ -10,16 +10,17 @@ from ..exceptions import UnexpectedTypeError
 WALLET = "Crypto.com Exchange"
 
 
-def parse_crypto_com_exchange(data_row, parser, _filename):
+def parse_crypto_com_exchange(data_row, parser, filename):
     in_row = data_row.in_row
     header = parser.header
-    timestamp = data_row.timestamp = DataParser.parse_timestamp(in_row[header.index('create_time_utc')])
+    timestamp = data_row.timestamp = DataParser.parse_timestamp(in_row[header.index('create_time_utc')]) if \
+        'create_time_utc' in header else DataParser.parse_timestamp(in_row[header.index('operateTime')])
     t_type = TransactionOutRecord.TYPE_TRADE
 
     if 'status' in header:
         status = in_row[header.index('status')]
         if 'stake_amount' in header:
-            TransactionOutRecord.TYPE_INTEREST
+            t_type = TransactionOutRecord.TYPE_INTEREST
             quantity = in_row[header.index('interest_amount')]
             asset = in_row[header.index('interest_currency')]
             if status == '1':
@@ -31,13 +32,26 @@ def parse_crypto_com_exchange(data_row, parser, _filename):
                                           in_row[header.index('status')])
         elif 'rebate_amount' in header:
             account = "FEE_REBATE"
-            TransactionOutRecord.TYPE_GIFT_RECEIVED
+            t_type = TransactionOutRecord.TYPE_GIFT_RECEIVED
             quantity = in_row[header.index('rebate_amount')]
             asset = in_row[header.index('rebate_currency')]
         else:
             account = "TRANSFER"
             quantity = in_row[header.index('amount')]
             asset = in_row[header.index('currency')]
+    elif 'historyInfo' in header:
+        account = "SYNDICATE"
+        asset = in_row[header.index('symbol')]
+        quantity = Decimal(in_row[header.index('amount')])
+        if "subscribe" in filename.lower():
+            quantity = quantity
+        elif "deliver" in filename.lower():
+            quantity = -quantity
+        elif "refund" in filename.lower():
+            quantity = -quantity
+        else:
+            raise UnexpectedTypeError(header.index('historyInfo'), 'historyInfo', in_row[header.index('historyInfo')])
+
     else:
         account = in_row[header.index('account_type')]
         quantity = in_row[header.index('traded_quantity')]
@@ -85,7 +99,17 @@ def parse_crypto_com_exchange(data_row, parser, _filename):
                                                      # fee_quantity=fee_quantity,
                                                      # fee_asset=fee_asset,
                                                      wallet=WALLET)
-
+    elif account == "SYNDICATE":
+        if quantity < 0:
+            data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT, timestamp,
+                                                     buy_quantity=abs(quantity),
+                                                     buy_asset=asset,
+                                                     wallet=WALLET)
+        else:
+            data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL, timestamp,
+                                                     sell_quantity=quantity,
+                                                     sell_asset=asset,
+                                                     wallet=WALLET)
     else:
         raise UnexpectedTypeError(0, parser.in_header[0], in_row[0])
 
@@ -117,3 +141,16 @@ DataParser(DataParser.TYPE_EXCHANGE,
            worksheet_name="Crypto.com Exchange R",
            row_handler=parse_crypto_com_exchange)
 
+# This one is required for a proper audit, but after one year of asking, CDC still didn't extend the export function to
+# the Syndicate Details page. So I will just use the exact data harvested from the XmlHttpRequests on the exchange
+# converted to csv. For example, like this:
+# # pip3 install pandas
+# # python3
+# import pandas as pd
+# for name in ('syndicate_delivered.json', 'syndicate_refunded.json', 'syndicate_subscribed.json'):
+#     pd.read_json(name).to_csv(name + '.csv', index=None, header=True)
+DataParser(DataParser.TYPE_EXCHANGE,
+           "Crypto.com Exchange Syndicate",
+           ['activityId', 'symbol', 'amount', 'historyInfo', 'operateTime', 'ctime', 'id', 'type', 'mtime', 'userId'],
+           worksheet_name="Crypto.com Exchange S",
+           row_handler=parse_crypto_com_exchange)
