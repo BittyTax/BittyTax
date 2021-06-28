BittyTax/BittyTax

commit 45b21005014d3483094b7a1d5f6910d4ad600c45

Conversion tool: added parser for Helium wallet and explorer
Added new parser for Helium wallet and explorer. Thankyou to @rsnodgrass for input.

This change also contains an update to the convert_currency method of DataParser to handle prices for todays date.

bittytax/conv/dataparser.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # (c) Nano Nano Ltd 2019
 
-import datetime
 import sys
 from decimal import Decimal
+from datetime import datetime
 
 from colorama import Fore, Style
 import dateutil.parser
@@ -97,7 +97,11 @@ class DataParser(object):
         if config.ccy == from_currency:
             return Decimal(value)
 
-        rate_ccy, _, _, _ = cls.price_data.get_historical(from_currency, config.ccy, timestamp)
+        if timestamp.date() >= datetime.now().date():
+            rate_ccy, _, _ = cls.price_data.get_latest(from_currency, config.ccy)
+        else:
+            rate_ccy, _, _, _ = cls.price_data.get_historical(from_currency, config.ccy, timestamp)
+
         value_in_ccy = Decimal(value) * rate_ccy
 
         if config.debug:

bittytax/conv/parsers/__init__.py
@@ -25,6 +25,7 @@ from . import exodus
 from . import gatehub
 from . import gravity
 from . import handcash
+from . import helium
 from . import hitbtc
 from . import hotbit
 from . import ii

bittytax/conv/parsers/helium.py
@@ -1,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# (c) Nano Nano Ltd 2021
+
+from ..out_record import TransactionOutRecord
+from ..dataparser import DataParser
+from ..exceptions import UnexpectedTypeError
+
+WALLET = 'Helium'
+
+def parse_helium_fairspot(data_row, parser, **_kwargs):
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(row_dict['date'])
+    amount_ccy = DataParser.convert_currency(row_dict['usd_amount'], 'USD', data_row.timestamp)
+    fee_ccy = DataParser.convert_currency(row_dict['usd_fee'], 'USD', data_row.timestamp)
+
+    if row_dict['type'] == 'rewards_v1':
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_MINING,
+                                                 data_row.timestamp,
+                                                 buy_quantity=row_dict['hnt_amount'],
+                                                 buy_asset='HNT',
+                                                 buy_value=amount_ccy,
+                                                 wallet=WALLET)
+    elif row_dict['type'] == 'payment_v2':
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                 data_row.timestamp,
+                                                 buy_quantity=row_dict['hnt_amount'],
+                                                 buy_asset='HNT',
+                                                 buy_value=amount_ccy,
+                                                 fee_quantity=row_dict['hnt_fee'],
+                                                 fee_asset='HNT',
+                                                 fee_value=fee_ccy,
+                                                 wallet=WALLET)
+    elif row_dict['type'] == 'payment_v1':
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL,
+                                                 data_row.timestamp,
+                                                 sell_quantity=row_dict['hnt_amount'],
+                                                 sell_asset='HNT',
+                                                 sell_value=amount_ccy,
+                                                 fee_quantity=row_dict['hnt_fee'],
+                                                 fee_asset='HNT',
+                                                 fee_value=fee_ccy,
+                                                 wallet=WALLET)
+    else:
+        raise UnexpectedTypeError(parser.in_header.index('type', 'type', row_dict['type']))
+
+def parse_helium_explorer(data_row, parser, **_kwargs):
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(row_dict['Date'])
+
+    if row_dict['Tag'] == 'mined':
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_MINING,
+                                                 data_row.timestamp,
+                                                 buy_quantity=row_dict['Received Quantity'],
+                                                 buy_asset=row_dict['Received Currency'],
+                                                 fee_quantity=row_dict['Fee Amount'],
+                                                 fee_asset=row_dict['Fee Currency'],
+                                                 wallet=WALLET)
+
+    elif row_dict['Tag'] == 'payment' and row_dict['Received Quantity']:
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                 data_row.timestamp,
+                                                 buy_quantity=row_dict['Received Quantity'],
+                                                 buy_asset=row_dict['Received Currency'],
+                                                 fee_quantity=row_dict['Fee Amount'],
+                                                 fee_asset=row_dict['Fee Currency'],
+                                                 wallet=WALLET)
+
+    elif row_dict['Tag'] == 'payment' and row_dict['Sent Quantity']:
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL,
+                                                 data_row.timestamp,
+                                                 sell_quantity=row_dict['Sent Quantity'],
+                                                 sell_asset=row_dict['Sent Currency'],
+                                                 fee_quantity=row_dict['Fee Amount'],
+                                                 fee_asset=row_dict['Fee Currency'],
+                                                 wallet=WALLET)
+    else:
+        raise UnexpectedTypeError(parser.in_header.index('Tag'), 'Tag', row_dict['Tag'])
+
+DataParser(DataParser.TYPE_WALLET,
+           'Helium',
+           ['block', 'date', 'type', 'transaction_hash', 'hnt_amount', 'hnt_fee',
+            'usd_oracle_price', 'usd_amount', 'usd_fee', 'payer', 'payee'],
+           worksheet_name='Helium',
+           row_handler=parse_helium_fairspot)
+
+DataParser(DataParser.TYPE_EXPLORER,
+           'Helium Explorer',
+           ['Date', 'Received Quantity', 'Received From', 'Received Currency',
+            'Sent Quantity', 'Sent To', 'Sent Currency', 'Fee Amount', 'Fee Currency',
+            'Tag', 'Note', 'Hotspot', 'Reward Type', 'Block', 'Hash'],
+           worksheet_name='Helium',
+           row_handler=parse_helium_explorer)

commit c6aad4f89ffdf1581c6d46b5116d8c63e1b0dba1

Fixed AttributeError: type object 'datetime.datetime' has no attribute 'datetime'
This bug was introduced by commit https://github.com/BittyTax/BittyTax/commit/45b21005014d3483094b7a1d5f6910d4ad600c45.

bittytax/conv/dataparser.py
@@ -67,7 +67,7 @@ class DataParser(object):
     @classmethod
     def parse_timestamp(cls, timestamp_str, tzinfos=None, tz=None, dayfirst=False, fuzzy=False):
         if isinstance(timestamp_str, int):
-            timestamp = datetime.datetime.utcfromtimestamp(timestamp_str)
+            timestamp = datetime.utcfromtimestamp(timestamp_str)
         else:
             timestamp = dateutil.parser.parse(timestamp_str,
                                               tzinfos=tzinfos, dayfirst=dayfirst, fuzzy=fuzzy)