b7f1e8187ffccd1e72fddedee3ef6edba3c46dc4
Dora Morolli <doruchan@gmail.com>
2021-11-13 11:24:41 +1300

    Add FIFO tax rule

--- bittytax/config.py
+++ bittytax/config.py
@@ -33,6 +33,7 @@ class Config(object):
     TAX_RULES_UK_COMPANY = ['UK_COMPANY_JAN', 'UK_COMPANY_FEB', 'UK_COMPANY_MAR', 'UK_COMPANY_APR',
                             'UK_COMPANY_MAY', 'UK_COMPANY_JUN', 'UK_COMPANY_JUL', 'UK_COMPANY_AUG',
                             'UK_COMPANY_SEP', 'UK_COMPANY_OCT', 'UK_COMPANY_NOV', 'UK_COMPANY_DEC']
+    TAX_RULES_FIFO = 'FIFO'                        
 
     TRADE_ASSET_TYPE_BUY = 0
     TRADE_ASSET_TYPE_SELL = 1
@@ -60,6 +61,7 @@ class Config(object):
         'data_source_fiat': DATA_SOURCE_FIAT,
         'data_source_crypto': DATA_SOURCE_CRYPTO,
         'coinbase_zero_fees_are_gifts': False,
+        'tax_rules': TAX_RULES_UK_INDIVIDUAL,
     }
 
     def __init__(self):
