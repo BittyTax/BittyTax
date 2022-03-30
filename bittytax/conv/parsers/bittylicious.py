cd3b8e9ad8a465fbe379e0560bffa34679e0f24d
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2022-03-30 14:25:47 +0100

    Bittylicious parser: updated for new export format
    Bittylicious have added a new export format which this parser now supports.

--- bittytax/conv/parsers/bittylicious.py
+++ bittytax/conv/parsers/bittylicious.py
@@ -1,54 +1,42 @@
 # -*- coding: utf-8 -*-
 # (c) Nano Nano Ltd 2022
 
-import re
-
 from ..out_record import TransactionOutRecord
 from ..dataparser import DataParser
-from ..exceptions import UnexpectedContentError
+from ..exceptions import UnexpectedTypeError
 
 WALLET = "Bittylicious"
 
 def parse_bittylicious(data_row, parser, **_kwargs):
     row_dict = data_row.row_dict
-    data_row.timestamp = DataParser.parse_timestamp(row_dict['Start Timestamp (UTC)'])
-    buy_quantity, buy_asset = split_asset(row_dict['Amount'])
-    if buy_quantity is None:
-        raise UnexpectedContentError(parser.in_header.index('Amount'), 'Amount',
-                                     row_dict['Amount'])
+    data_row.timestamp = DataParser.parse_timestamp(row_dict['startedTime'])
 
-    sell_quantity, sell_asset = get_currency(row_dict['Fiat'])
-    if sell_quantity is None:
-        raise UnexpectedContentError(parser.in_header.index('Fiat'), 'Fiat', row_dict['Fiat'])
+    if row_dict['status'] != "RECEIVED":
+        return
 
-    if row_dict['Status'] == "Received":
+    if row_dict['direction'] == "BUY":
         data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_TRADE,
                                                  data_row.timestamp,
-                                                 buy_quantity=buy_quantity,
-                                                 buy_asset=buy_asset,
-                                                 sell_quantity=sell_quantity.replace(',', ''),
-                                                 sell_asset=sell_asset,
+                                                 buy_quantity=row_dict['coinAmount'],
+                                                 buy_asset=row_dict['coin'],
+                                                 sell_quantity=row_dict['fiatCurrencyAmount'],
+                                                 sell_asset=row_dict['fiatCurrency'],
                                                  wallet=WALLET)
-
-def split_asset(amount):
-    match = re.match(r'^(\d+|\d+\.\d+) (\w+)$', amount)
-    if match:
-        return match.group(1), match.group(2)
-    return None, ''
-
-def get_currency(fiat):
-    match = re.match(r'^-?([£€])([\d|,]+(?:\.\d{2})?)$', fiat)
-    if match:
-        if match.group(1) == '£':
-            return match.group(2), "GBP"
-
-        if match.group(1) == '€':
-            return match.group(2), "EUR"
-
-    return None, ''
+    elif row_dict['direction'] == "SELL":
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_TRADE,
+                                                 data_row.timestamp,
+                                                 buy_quantity=row_dict['fiatCurrencyAmount'],
+                                                 buy_asset=row_dict['fiatCurrency'],
+                                                 sell_quantity=row_dict['coinAmount'],
+                                                 sell_asset=row_dict['coin'],
+                                                 wallet=WALLET)
+    else:
+        raise UnexpectedTypeError(parser.in_header.index('direction'), 'direction',
+                                  row_dict['direction'])
 
 DataParser(DataParser.TYPE_EXCHANGE,
            "Bittylicious",
-           ['Reference', 'Start Timestamp (UTC)', 'Status', 'Amount', 'Fiat'],
+           ['reference', 'direction', 'status', 'coin', 'coinAmount', 'fiatCurrency',
+            'fiatCurrencyAmount', 'startedTime', 'endedTime', 'transactionID', 'coinAddress'],
            worksheet_name="Bittylicious",
            row_handler=parse_bittylicious)
