585a6feff977473de53e87aeeb88e684f6bcf1fe
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2023-03-21 11:29:38 +0000

    OKX parser: added new export formats

--- src/bittytax/conv/parsers/okx.py
+++ src/bittytax/conv/parsers/okx.py
@@ -9,14 +9,89 @@ from colorama import Fore
 
 from ...config import config
 from ..dataparser import DataParser
-from ..exceptions import DataRowError, UnexpectedTypeError
+from ..exceptions import DataRowError, UnexpectedContentError, UnexpectedTypeError
 from ..out_record import TransactionOutRecord
 
-WALLET = "OKEx"
+WALLET = "OKX"
 TZ_INFOS = {"CST": dateutil.tz.gettz("Asia/Shanghai")}
 
 
-def parse_okex_trades(data_rows, parser, **_kwargs):
+def parse_okx_trades_v2(data_rows, parser, **_kwargs):
+    ids = {}
+
+    for dr in data_rows:
+        if dr.row_dict["id"] in ids:
+            ids[dr.row_dict["id"]].append(dr)
+        else:
+            ids[dr.row_dict["id"]] = [dr]
+
+    for data_row in data_rows:
+        if config.debug:
+            sys.stderr.write(
+                "%sconv: row[%s] %s\n"
+                % (Fore.YELLOW, parser.in_header_row_num + data_row.line_num, data_row)
+            )
+
+        if data_row.parsed:
+            continue
+
+        try:
+            _parse_okx_trades_v2_row(ids, parser, data_row)
+        except DataRowError as e:
+            data_row.failure = e
+
+
+def _parse_okx_trades_v2_row(ids, parser, data_row):
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(row_dict["Time"])
+    data_row.parsed = True
+
+    if row_dict["Trade Type"] == "Spot":
+        if row_dict["Type"] in ("Buy", "Sell"):
+            _make_trade(ids[row_dict["id"]], data_row, parser)
+        else:
+            raise UnexpectedTypeError(
+                parser.in_header.index("Type"), "Type", data_row.row_dict["Type"]
+            )
+    elif row_dict["Trade Type"] == "Transfer":
+        # Skip internal transfers
+        return
+    else:
+        raise UnexpectedTypeError(
+            parser.in_header.index("Trade Type"), "Trade Type", data_row.row_dict["Trade Type"]
+        )
+
+
+def _make_trade(ids, data_row, parser):
+    buy_rows = [dr for dr in ids if dr.row_dict["Type"] == "Buy"]
+    sell_rows = [dr for dr in ids if dr.row_dict["Type"] == "Sell"]
+
+    if len(buy_rows) == 1 and len(sell_rows) == 1:
+        if data_row == buy_rows[0]:
+            sell_rows[0].timestamp = data_row.timestamp
+            sell_rows[0].parsed = True
+        else:
+            buy_rows[0].timestamp = data_row.timestamp
+            buy_rows[0].parsed = True
+
+        data_row.t_record = TransactionOutRecord(
+            TransactionOutRecord.TYPE_TRADE,
+            data_row.timestamp,
+            buy_quantity=buy_rows[0].row_dict["Amount"],
+            buy_asset=buy_rows[0].row_dict["Unit"],
+            sell_quantity=sell_rows[0].row_dict["Amount"],
+            sell_asset=sell_rows[0].row_dict["Unit"],
+            fee_quantity=abs(Decimal(buy_rows[0].row_dict["Fee"])),
+            fee_asset=buy_rows[0].row_dict["Unit"],
+            wallet=WALLET,
+        )
+    else:
+        data_row.failure = UnexpectedContentError(
+            parser.in_header.index("Type"), "Type", data_row.row_dict["Type"]
+        )
+
+
+def parse_okx_trades_v1(data_rows, parser, **_kwargs):
     for buy_row, sell_row in zip(data_rows[0::2], data_rows[1::2]):
         try:
             if config.debug:
@@ -37,12 +112,12 @@ def parse_okex_trades(data_rows, parser, **_kwargs):
                     )
                 )
 
-            _parse_okex_trades_row(buy_row, sell_row, parser)
+            _parse_okx_trades_v1_row(buy_row, sell_row, parser)
         except DataRowError as e:
             buy_row.failure = e
 
 
-def _parse_okex_trades_row(buy_row, sell_row, parser):
+def _parse_okx_trades_v1_row(buy_row, sell_row, parser):
     buy_row.timestamp = DataParser.parse_timestamp(buy_row.row_dict["time"], tzinfos=TZ_INFOS)
     sell_row.timestamp = DataParser.parse_timestamp(sell_row.row_dict["time"], tzinfos=TZ_INFOS)
 
@@ -62,10 +137,66 @@ def _parse_okex_trades_row(buy_row, sell_row, parser):
         raise UnexpectedTypeError(parser.in_header.index("type"), "type", buy_row.row_dict["type"])
 
 
+def parse_okx_funding(data_row, parser, **_kwargs):
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(row_dict["Time"])
+
+    if row_dict["Type"] == "Staking Yield":
+        data_row.t_record = TransactionOutRecord(
+            TransactionOutRecord.TYPE_STAKING,
+            data_row.timestamp,
+            buy_quantity=row_dict["Amount"],
+            buy_asset=row_dict["Symbol"],
+            wallet=WALLET,
+        )
+    elif row_dict["Type"] in (
+        "Stake",
+        "Redeem staking",
+        "Savings subscription",
+        "Savings redemption",
+    ):
+        # Skip not taxable events
+        return
+    else:
+        raise UnexpectedTypeError(parser.in_header.index("Type"), "Type", row_dict["Type"])
+
+
 DataParser(
     DataParser.TYPE_EXCHANGE,
-    "OKEx",
+    "OKX Trades",
+    [
+        "id",
+        "Order id",
+        "Time",
+        "Trade Type",
+        "Instrument",
+        "Type",
+        "Amount",
+        "Unit",
+        "PL",
+        "Fee",
+        "Position Change",
+        "Position Balance",
+        "Balance Change",
+        "Balance",
+        "Unit",
+    ],
+    worksheet_name="OKX T",
+    all_handler=parse_okx_trades_v2,
+)
+
+DataParser(
+    DataParser.TYPE_EXCHANGE,
+    "OKX Trades",
     ["time", "type", "size", "balance", "fee", "currency"],
-    worksheet_name="OKEx",
-    all_handler=parse_okex_trades,
+    worksheet_name="OKX T",
+    all_handler=parse_okx_trades_v1,
+)
+
+DataParser(
+    DataParser.TYPE_EXCHANGE,
+    "OKX Funding",
+    ["id", "", "Time", "Type", "Amount", "Before Balance", "After Balance", "Fee", "Symbol"],
+    worksheet_name="OKX F",
+    row_handler=parse_okx_funding,
 )
