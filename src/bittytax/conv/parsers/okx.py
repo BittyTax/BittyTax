dcdc73d09558dc66d9e75344a29b567e85a152fb
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2023-03-22 22:47:12 +0000

    OKX parser: last correction (fingers crossed)

--- src/bittytax/conv/parsers/okx.py
+++ src/bittytax/conv/parsers/okx.py
@@ -18,7 +18,7 @@ TZ_INFOS = {"CST": dateutil.tz.gettz("Asia/Shanghai")}
 if sys.version_info[0] < 3:
     BOM = "\xef\xbb\xbf"
 else:
-    BOM = u"\ufeff"
+    BOM = "\ufeff"  # pylint: disable=anomalous-unicode-escape-in-string
 
 
 def parse_okx_trades_v2(data_rows, parser, **_kwargs):
