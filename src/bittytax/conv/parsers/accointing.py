d018894b5d3516c6f98793c91a482d3f35d3b199
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2024-06-30 12:14:58 +0100

    Accointing parser: added different export format

--- src/bittytax/conv/parsers/accointing.py
+++ src/bittytax/conv/parsers/accointing.py
@@ -127,7 +127,7 @@ DataParser(
         "classification",
         "walletName",
         "walletProvider",
-        "providerId",  # New field
+        "providerId",
         "txId",
         "primaryAddress",
         "otherAddress",
@@ -141,6 +141,41 @@ DataParser(
         "comments",
         "fiatValueOverwrite",  # New field
         "feeFiatValueOverwrite",  # New field
+    ],
+    worksheet_name="Accointing",
+    row_handler=parse_accointing,
+)
+
+DataParser(
+    ParserType.ACCOUNTING,
+    "Accointing",
+    [
+        "timeExecuted",
+        "type",
+        "boughtQuantity",
+        "boughtCurrency",
+        "boughtCurrencyId",
+        "soldQuantity",
+        "soldCurrency",
+        "soldCurrencyId",
+        "feeQuantity",
+        "feeCurrency",
+        "feeCurrencyId",
+        "classification",
+        "walletName",
+        "walletProvider",
+        "providerId",  # New field
+        "txId",
+        "primaryAddress",
+        "otherAddress",
+        "temporaryCurrencyName",
+        "temporaryFeeCurrencyName",
+        "temporaryBoughtCurrencyTicker",
+        "temporarySoldCurrencyTicker",
+        "temporaryFeeCurrencyTicker",
+        "id",
+        "associatedTransferId",
+        "comments",
     ],
     worksheet_name="Accointing",
     row_handler=parse_accointing,
