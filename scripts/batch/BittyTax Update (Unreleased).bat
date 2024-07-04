a9f526de4c820b7f8e35918bb90b43afc729a029
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2024-07-04 16:59:29 +0100

    Scripts: updates for US branch

--- scripts/batch/BittyTax Update (Unreleased).bat
+++ scripts/batch/BittyTax Update (Unreleased).bat
@@ -3,5 +3,5 @@
 :: This batch script installs the latest "unreleased" version of BittyTax from GitHub, see https://github.com/BittyTax/BittyTax.
 :: * You must have previously installed the official release.
 ::
-pip install --force-reinstall --no-deps https://github.com/BittyTax/BittyTax/archive/refs/heads/master.zip
+pip install --force-reinstall --no-deps https://github.com/BittyTax/BittyTax/archive/refs/heads/features/usa-tax-rules.zip
 @pause
