58aeabe484cd5a8a2aab12ab8b62b4e4eec46ef7
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2025-09-27 15:18:42 +0100

    Scripts moved to BittyTax-Premium for sponsors only

--- scripts/batch/BittyTax Install (Latest Release).bat
+++
@@ -1,6 +0,0 @@
-:: BittyTax Install (Latest Release)
-::
-:: This batch script installs (or upgrades) to the latest official release of BittyTax as stored on the Python Package Index (PyPI), see https://pypi.org/project/BittyTax/.
-::
-pip install BittyTax --upgrade
-@pause
--- scripts/batch/BittyTax Update (Unreleased).bat
+++
@@ -1,7 +0,0 @@
-:: BittyTax Update (Unreleased)
-::
-:: This batch script installs the latest "unreleased" version of BittyTax from GitHub, see https://github.com/BittyTax/BittyTax.
-:: * You must have previously installed the official release.
-::
-pip install --force-reinstall --no-deps https://github.com/BittyTax/BittyTax/archive/refs/heads/features/usa-tax-rules.zip
-@pause
