fef3f7ea9d919b9031863a3d6c913a35ff9acac2
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2025-09-27 15:18:42 +0100

    Scripts moved to BittyTax-Premium for sponsors only

--- scripts/powershell/BittyTax Accounting Tool.bat
+++
@@ -1,7 +0,0 @@
-:: BittyTax Accounting Tool
-::
-:: This script lets you drag & drop a BittyTax records file (.xlsx) to be processed by the Accounting Tool.
-::
-@echo off
-set PSScript=%~dpn0.ps1
-powershell.exe -ExecutionPolicy Bypass -File "%PSScript%" %*
