f327534f537b40dcb14b1f27a90cd4c77740325b
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2025-10-20 23:35:45 +0100

    Added spoof packages to remove dependancies
    1. python-bidi is not yet supported in Python 3.14 and is required by xhtml2pdf.
    2. xhtml2pdf requires svglib -> rlPyCairo -> pycairo which needs to be compiled and built with the cairo library.
    
    Neither of these packages is actually required for the PDF generation of BittyTax reports.

---
+++ src/python-bidi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+    setup(
+        name="python-bidi",
+        version="0.6.6",
+        packages=find_packages(),
+        description="Spoof implementation of python-bidi for xhtml2pdf",
+    )
