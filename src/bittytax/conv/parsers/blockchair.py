7ff3be91f29c4ea2496b73b850da9c700d97128d
Ravi Deo <50364452+deoscode@users.noreply.github.com>
2026-05-17 16:52:36 +0100

    fixed line too long

--- src/bittytax/conv/parsers/blockchair.py
+++ src/bittytax/conv/parsers/blockchair.py
@@ -71,7 +71,8 @@ def parse_blockchair_extended(
             wallet=WALLET,
         )
     else:
-        raise UnexpectedTypeError(parser.in_header.index("Tx number"), "Tx number", row_dict["Tx number"])
+        raise UnexpectedTypeError(parser.in_header.index("Tx number"), 
+                                  "Tx number", row_dict["Tx number"])
 
 
 # simple format
